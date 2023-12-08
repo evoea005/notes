d9a3b86b3d2deb584c6ad9f9b5557768481d91f8
evoea005 <evoea005@osloskolen.no>
2023-12-08 13:14:32 +0100

    oh

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 session_start();
 
 if (!isset($_SESSION["username"])) {
-	header("Location: /auth/login");
+	header("Location: /auth/login.php");
 } else {
-	header("Location: /editor/editor");
+	header("Location: /editor/editor.php");
 }
 
 ?>
